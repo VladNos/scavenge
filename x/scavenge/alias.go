5ae711745a15951e5d5817949b7c4bc9a019e374
vladnos <vladpersa@gmail.com>
2020-06-01 00:58:48 +0300

    original try

---
+++ x/scavenge/alias.go
@@ -0,0 +1,44 @@
+package scavenge
+
+import (
+	"github.com/vladnos/scavenge/x/scavenge/keeper"
+	"github.com/vladnos/scavenge/x/scavenge/types"
+)
+
+const (
+	// TODO: define constants that you would like exposed from your module
+
+	ModuleName        = types.ModuleName
+	RouterKey         = types.RouterKey
+	StoreKey          = types.StoreKey
+	DefaultParamspace = types.DefaultParamspace
+	QueryParams       = types.QueryParams
+	QuerierRoute      = types.QuerierRoute
+)
+
+var (
+	// functions aliases
+	NewKeeper           = keeper.NewKeeper
+	NewQuerier          = keeper.NewQuerier
+	RegisterCodec       = types.RegisterCodec
+	NewGenesisState     = types.NewGenesisState
+	DefaultGenesisState = types.DefaultGenesisState
+	ValidateGenesis     = types.ValidateGenesis
+	// TODO: Fill out function aliases
+
+	// variable aliases
+	ModuleCdc     = types.ModuleCdc
+	NewMsgCreateScavenge = types.NewMsgCreateScavenge
+	NewMsgCommitSolution = types.NewMsgCommitSolution
+	NewMsgRevealSolution = types.NewMsgRevealSolution
+)
+
+type (
+	Keeper       = keeper.Keeper
+	GenesisState = types.GenesisState
+	Params       = types.Params
+
+	MsgCreateScavenge = types.MsgCreateScavenge
+	MsgCommitSolution = types.MsgCommitSolution
+	MsgRevealSolution = types.MsgRevealSolution
+)
