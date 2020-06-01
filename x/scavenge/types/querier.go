15ec0999a562ae7994906debb447c3016f5d58cd
unknown <vladpersa@gmail.com>
2020-06-01 16:58:26 +0300

    K

--- x/scavenge/types/querier.go
+++ x/scavenge/types/querier.go
@@ -5,18 +5,25 @@ package types
 const (
 	// TODO: Describe query parameters, update <action> with your query
 	// Query<Action>    = "<action>"
+	QueryListScavenges = "list"
+	QueryGetScavenge   = "get"
+	QueryCommit        = "commit"
 )
 
-/* 
+
 Below you will be able how to set your own queries:
-
-
-// QueryResList Queries Result Payload for a query
+type QueryResScavenges []string
 type QueryResList []string
+// QueryResList Queries Result Payload for a query
+
+
 
 // implement fmt.Stringer
 func (n QueryResList) String() string {
 	return strings.Join(n[:], "\n")
 }
+
+func (n QueryResScavenges) String() string {
+	return strings.Join(n[:], "\n")
 
 */
